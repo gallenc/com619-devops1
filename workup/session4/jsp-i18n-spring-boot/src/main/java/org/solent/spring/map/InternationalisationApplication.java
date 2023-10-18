6807b7c0719578e48c6ac31422e26e0bcad815b4
gallenc <cgallen@opennms.org>
2023-10-18 08:37:56 +0100

    spring boot internationalisation - simple

---
+++ workup/session4/jsp-i18n-spring-boot/src/main/java/org/solent/spring/map/InternationalisationApplication.java
@@ -0,0 +1,15 @@
+package org.solent.spring.map;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class InternationalisationApplication extends SpringBootServletInitializer {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternationalisationApplication.class, args);
+	}
+}
